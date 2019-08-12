7fcc55c78a6ad2b63ba5bd76ca81144357916f35
Taryn <taryn@Apples-MacBook-Pro.local>
2019-08-12 10:18:06 -0400

    Emojinal

---
+++ Emojinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojinal
+//
+//  Created by Taryn on 8/12/19.
+//  Copyright © 2019 Taryn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
